c220e52ad7a7aa9c00ac2484f91600dfab940467
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-11-06 16:10:47 +0500

    added static root

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Define the base URL for serving media files
 MEDIA_URL = '/media/'
