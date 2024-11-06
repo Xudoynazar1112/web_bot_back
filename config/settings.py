b990810f2c644d7c6f19c66f96d3b923705ce5d2
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-11-06 16:14:22 +0500

    updated allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j&(ouyibgq@rpr4@486q2*u_k=s6mj%5pn&eji58@fcw=(peuz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
