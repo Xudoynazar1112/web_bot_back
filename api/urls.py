ea2b9b791732ae1b94520340ea12d3ef8e2ee3e5
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-11-06 16:04:31 +0500

    added crud and login endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ProductListAPIView
+from .views import ProductListAPIView, ProductDeleteAPIView, ProductDetailAPIView, ProductUpdateAPIView, \
+    ProductCreateAPIView
 
 urlpatterns = [
-    path('', ProductListAPIView.as_view(), name='product_list')
+    path('', ProductListAPIView.as_view(), name='list'),
+    path('delete/<int:pk>/', ProductDeleteAPIView.as_view(), name='delete'),
+    path('detail/<int:pk>/', ProductDetailAPIView.as_view(), name='detail'),
+    path('update/<int:pk>/', ProductUpdateAPIView.as_view(), name='update'),
+    path('create/', ProductCreateAPIView.as_view(), name='create'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,28 @@
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 
 
 class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCreateAPIView(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductUpdateAPIView(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDeleteAPIView(DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
